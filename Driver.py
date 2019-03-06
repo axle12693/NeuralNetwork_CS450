291a2c743a921353b450d28c6c03263e499999df
axle12693 <axle12693@gmail.com>
2019-03-05 21:29:31 -0700

    Added backprop - doesn't fully work yet

--- Driver.py
+++ Driver.py
@@ -15,13 +15,64 @@
 #
 # print(calc([1,5]))
 
+import numpy as np
 
 import Topology
 import Network
-top = Topology.Topology()
-top.add_layer(2, "Input")
-top.add_layer(2)
-top.add_layer(1)
-net = Network.Network(top)
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
-print(net.predict([4,5]))
+def print_error(net, x_test, y_test):
+    total_error = 0
+    n = 0
+    for i in range(len(x_test)):
+        total_error += int(np.argmax(net.predict(x_test[i])) != np.argmax(y_test[i]))
+        n += 1
+    error = total_error / n
+
+    print("Error: " + str(error))
+
+print("Loading and prepping data...")
+
+iris = load_iris()
+data = iris["data"]
+
+data = np.transpose(data)
+for index, column in enumerate(data):
+    column_mean = np.mean(column)
+    column_std = np.std(column)
+    data[index] = (column - column_mean) / column_std
+
+data = np.transpose(data)
+data = np.ndarray.tolist(data)
+
+targets = iris["target"]
+targets_lists = []
+for target in targets:
+    target_list = [0, 0, 0]
+    target_list[target] = 1
+    targets_lists.append(target_list)
+x_train, x_test, y_train, y_test = train_test_split(data, targets_lists, test_size=0.3)
+print("Creating network...")
+top = Topology.Topology()
+top.add_layer(4, "Input")
+top.add_layer(5)
+top.add_layer(5)
+top.add_layer(5)
+top.add_layer(4)
+top.add_layer(3)
+net = Network.Network(top, learning_rate=0.01)
+print("training network...")
+
+print_error(net, x_test, y_test)
+
+net.fit(x_train, y_train, 1000)
+
+print_error(net, x_test, y_test)
+
+print()
+
+
+for i in range(len(x_test)):
+    print("My prediction: " + str(net.predict(x_test[i])))
+    print("Target: " + str(y_test[i]))
--- Network.py
+++ Network.py
@@ -1,11 +1,15 @@
 import random
 import numpy as np
+from copy import deepcopy
 
 
 class Network:
-    def __init__(self, topology):
+    def __init__(self, topology, learning_rate=0.1):
         self.topology = topology
         self.weights = []
+        self.activations = []
+        self.errors = []
+        self.learning_rate = learning_rate
 
         for layer_index in range(len(topology.layers) - 1):
             self.weights.append([])
@@ -14,16 +18,64 @@ class Network:
                 self.weights[layer_index][k_neuron].append(random.uniform(-1.0, 1.0))
                 for j_neuron in range(topology.layers[layer_index][1]):
                     self.weights[layer_index][k_neuron].append(random.uniform(-1.0, 1.0))
+        self.weights_copy = deepcopy(self.weights)  # enables us to avoid altering the weights until after all neuron
+                                                    # errors are calculated
 
-        print()
-    def predict(self, data):
-        next_layer_input = np.array(data)
+    def predict(self, row):
+        self.activations = [row]
+        next_layer_input = np.array(row)
         for index in range(len(self.topology.layers)-1):
             next_layer_input = np.concatenate(([-1], next_layer_input))
             next_layer_input = np.array(self.weights[index]).dot(next_layer_input)
             next_layer_input = self.topology.layers[index+1][0](next_layer_input)
+            self.activations.append(next_layer_input)
+
         return next_layer_input
 
+    def fit(self, data, targets, num_epochs=100):
+        for i in range(num_epochs):
+            print("Epoch " + str(i))
+            for index, row in enumerate(data):
+                self.errors = []
+                self.predict(row)
+                output_layer_calculated = False
+                for neuron_layer_index in range(len(self.activations)-1, -1, -1):
+                    if not output_layer_calculated:
+                        self.calc_errors_and_update_weights(neuron_layer_index, targets[index])
+                        output_layer_calculated = True
+                    else:
+                        self.calc_errors_and_update_weights(neuron_layer_index)
+                self.weights = deepcopy(self.weights_copy)
 
-
+    def calc_errors_and_update_weights(self, neuron_layer_index, targets=[]):
+        """Update weights feeding into the referenced layer of neurons."""
+        if neuron_layer_index == 0:
+            return
+        if len(targets) > 0: #if neuron_layer_index is the output layer
+            neuron_layer_errors = []
+            for j_neuron_index in range(len(self.activations[neuron_layer_index])):
+                activation = self.activations[neuron_layer_index][j_neuron_index]
+                error = activation * (1 - activation) * (activation - targets[j_neuron_index])
+                neuron_layer_errors.append(error)
+                for i_neuron_index in range(len(self.activations[neuron_layer_index - 1])):
+                    i_neuron_activation = self.activations[neuron_layer_index - 1][i_neuron_index]
+                    amount_to_change_weight = self.learning_rate * error * i_neuron_activation
+                    self.weights_copy[neuron_layer_index - 1][j_neuron_index][i_neuron_index] -= amount_to_change_weight
+                    #print(amount_to_change_weight)
+            self.errors = [neuron_layer_errors] + self.errors
+        else:
+            neuron_layer_errors = []
+            for j_neuron_index in range(len(self.activations[neuron_layer_index])):
+                activation = self.activations[neuron_layer_index][j_neuron_index]
+                weighted_sum_of_previous_errors = 0
+                for k_neuron_index in range(len(self.activations[neuron_layer_index + 1])):
+                    weighted_sum_of_previous_errors += self.errors[0][k_neuron_index]*self.weights[neuron_layer_index][k_neuron_index][j_neuron_index]
+                error = activation * (1 - activation) * weighted_sum_of_previous_errors
+                neuron_layer_errors.append(error)
+                for i_neuron_index in range(len(self.activations[neuron_layer_index - 1])):
+                    i_neuron_activation = self.activations[neuron_layer_index - 1][i_neuron_index]
+                    amount_to_change_weight = self.learning_rate * error * i_neuron_activation
+                    self.weights_copy[neuron_layer_index - 1][j_neuron_index][i_neuron_index] -= amount_to_change_weight
+                    #print(amount_to_change_weight)
+            self.errors = [neuron_layer_errors] + self.errors
 
