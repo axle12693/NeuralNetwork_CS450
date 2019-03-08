5a9117ab298a7f16cb677e2362e31df22cdb3964
axle12693 <axle12693@gmail.com>
2019-03-07 19:02:55 -0700

    Finished backprop. Started genetic algorithm.

--- Driver.py
+++ Driver.py
@@ -1,38 +1,20 @@
-# import numpy as np
-#
-# def calc(ls):
-#     neuron_output_layer_1 = np.array(ls)
-#     weights_layer1 = np.array([[1,3],[2,4]])
-#     neuron_input_layer_2 = weights_layer1.dot(neuron_output_layer_1)
-#     print(neuron_input_layer_2)
-#     neuron_output_layer_2 = np.maximum(neuron_input_layer_2, 0)
-#     weights_layer_2 = np.array([[5, 6]])
-#     neuron_input_layer_3 = weights_layer_2.dot(neuron_output_layer_2)
-#     neuron_output_layer_3 = np.maximum(neuron_input_layer_3, 0)
-#
-#     return neuron_output_layer_3
-#
-#
-# print(calc([1,5]))
-
 import numpy as np
-
 import Topology
 import Network
 from sklearn.datasets import load_iris
+from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
-def print_error(net, x_test, y_test):
-    total_error = 0
-    n = 0
-    for i in range(len(x_test)):
-        total_error += int(np.argmax(net.predict(x_test[i])) != np.argmax(y_test[i]))
-        n += 1
-    error = total_error / n
 
-    print("Error: " + str(error))
+def fitness_callback(net, x_test, y_test):
+    test_error = 0
+    for j in range(len(x_test)):
+        test_error += (net.predict(x_test[j]) - y_test[j]) ** 2
+    test_error = np.sum(test_error) / len(x_test)
+    return 1 - test_error
 
-print("Loading and prepping data...")
+print("Iris dataset:")
 
 iris = load_iris()
 data = iris["data"]
@@ -53,26 +35,115 @@ for target in targets:
     target_list[target] = 1
     targets_lists.append(target_list)
 x_train, x_test, y_train, y_test = train_test_split(data, targets_lists, test_size=0.3)
-print("Creating network...")
 top = Topology.Topology()
 top.add_layer(4, "Input")
 top.add_layer(5)
 top.add_layer(5)
-top.add_layer(5)
-top.add_layer(4)
 top.add_layer(3)
 net = Network.Network(top, learning_rate=0.01)
-print("training network...")
 
-print_error(net, x_test, y_test)
+plot_data = net.fit(x_train, y_train, x_test, y_test, 1000)
 
-net.fit(x_train, y_train, 1000)
+plt.plot(plot_data[0], plot_data[1], label="Train")
+plt.plot(plot_data[0], plot_data[2], label="Test")
+plt.xlabel = "Iteration"
+plt.ylabel = "Error"
+plt.title = "Diabetes"
+plt.text = "Diabetes"
+plt.legend()
+plt.show()
 
-print_error(net, x_test, y_test)
 
 print()
 
+print("Diabetes dataset:")
 
-for i in range(len(x_test)):
-    print("My prediction: " + str(net.predict(x_test[i])))
-    print("Target: " + str(y_test[i]))
+diabetes = load_diabetes()
+
+data = diabetes["data"]
+targets = diabetes["target"]
+
+data = np.transpose(data)
+for index, column in enumerate(data):
+    column_mean = np.mean(column)
+    column_std = np.std(column)
+    data[index] = (column - column_mean) / column_std
+
+data = np.transpose(data)
+data = np.ndarray.tolist(data)
+
+
+target_mean = np.mean(targets)
+target_std = np.std(targets)
+targets = (targets - 25) / (346-25)
+targets = np.ndarray.tolist(targets)
+for i in range(len(targets)):
+    targets[i] = [targets[i]]
+
+
+x_train, x_test, y_train, y_test = train_test_split(data, targets, test_size=0.3)
+
+top = Topology.Topology()
+top.add_layer(10, "Input")
+top.add_layer(6)
+top.add_layer(1)
+net = Network.Network(top, 0.01)
+
+
+plot_data = net.fit(x_train, y_train, x_test, y_test, 1000)
+plt.plot(plot_data[0], plot_data[1], label="Train")
+plt.plot(plot_data[0], plot_data[2], label="Test")
+plt.xlabel = "Iteration"
+plt.ylabel = "Error"
+plt.title = "Diabetes"
+plt.text = "Diabetes"
+plt.legend()
+plt.show()
+
+# The following is from a kaggle dataset. It shrinks the amount of data it has to sift through,
+# and it still ran for about 90 minutes. Trained very badly.
+
+# print()
+#
+# print("Santander:")
+#
+# # load train data
+# print("Loading training data...")
+# data = np.genfromtxt("train.csv", delimiter=',')
+# data = np.delete(data, 0, axis=0)
+# data = np.transpose(data)
+# data = np.delete(data, 0, axis=0)
+# x_train = np.delete(data, 0, axis=0)
+# y_train = data[0]
+# for index, column in enumerate(x_train):
+#     column_mean = np.mean(column)
+#     column_std = np.std(column)
+#     x_train[index] = (column - column_mean) / column_std
+# x_train = np.transpose(x_train)
+# y_train = np.ndarray.tolist(y_train)
+# for i in range(len(y_train)):
+#     y_train[i] = [y_train[i]]
+#
+# x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.99)
+# x_test, garbage, y_test, garbage = train_test_split(x_test, y_test, test_size=0.99)
+#
+# print("Creating the network...")
+# top = Topology.Topology()
+# top.add_layer(200, "Input")
+# top.add_layer(100)
+# top.add_layer(50)
+# top.add_layer(25)
+# top.add_layer(10)
+# top.add_layer(5)
+# top.add_layer(1)
+# net = Network.Network(top,0.01)
+# print("Fitting...")
+# plot_data = net.fit(x_train, y_train, x_test, y_test, 100)
+# plt.plot(plot_data[0], plot_data[1], label="Train")
+# plt.plot(plot_data[0], plot_data[2], label="Test")
+# plt.xlabel = "Iteration"
+# plt.ylabel = "Error"
+# plt.title = "Diabetes"
+# plt.text = "Diabetes"
+# plt.legend()
+# plt.show()
--- Network.py
+++ Network.py
@@ -22,30 +22,136 @@ class Network:
                                                     # errors are calculated
 
     def predict(self, row):
-        self.activations = [row]
+        self.activations = [[-1] + row]
         next_layer_input = np.array(row)
         for index in range(len(self.topology.layers)-1):
             next_layer_input = np.concatenate(([-1], next_layer_input))
             next_layer_input = np.array(self.weights[index]).dot(next_layer_input)
             next_layer_input = self.topology.layers[index+1][0](next_layer_input)
-            self.activations.append(next_layer_input)
+            if index == len(self.topology.layers)-2:
+                self.activations.append(next_layer_input)
+            else:
+                self.activations.append(np.concatenate(([-1], next_layer_input)))
 
         return next_layer_input
 
-    def fit(self, data, targets, num_epochs=100):
-        for i in range(num_epochs):
-            print("Epoch " + str(i))
-            for index, row in enumerate(data):
-                self.errors = []
-                self.predict(row)
-                output_layer_calculated = False
-                for neuron_layer_index in range(len(self.activations)-1, -1, -1):
-                    if not output_layer_calculated:
-                        self.calc_errors_and_update_weights(neuron_layer_index, targets[index])
-                        output_layer_calculated = True
-                    else:
-                        self.calc_errors_and_update_weights(neuron_layer_index)
+    def fit(self, data, targets, test_data, test_targets, num_epochs=100, method="backprop", crossover_rate=0.02, mutation_rate = 0.02, population_size=100, fitness_callback=None):
+        plot_details = [[],[],[]]
+        if method == "backprop":
+            for i in range(num_epochs):
+                #if i % int(num_epochs / 10) == 0:
+                print("Epoch " + str(i))
+                for index, row in enumerate(data):
+                    if len(data) > 5000 and index % (len(data) // 1000) == 0:
+                        print("Beginning row " + str(index) + " of " + str(len(data)))
+                    self.errors = []
+                    self.predict(row)
+                    output_layer_calculated = False
+                    for neuron_layer_index in range(len(self.activations)-1, -1, -1):
+                        if not output_layer_calculated:
+                            self.calc_errors_and_update_weights(neuron_layer_index, targets[index])
+                            output_layer_calculated = True
+                        else:
+                            self.calc_errors_and_update_weights(neuron_layer_index)
                 self.weights = deepcopy(self.weights_copy)
+
+                train_error = 0
+                test_error = 0
+                for j in range(len(data)):
+                    train_error += (self.predict(data[j]) - targets[j]) ** 2
+                train_error = np.sum(train_error) / len(data)
+                for j in range(len(test_data)):
+                    test_error += (self.predict(test_data[j]) - test_targets[j]) ** 2
+                test_error = np.sum(test_error) / len(test_data)
+
+                plot_details[0].append(i)
+                plot_details[1].append(train_error)
+                plot_details[2].append(test_error)
+            return plot_details
+        elif method == "genetic":
+            print("Beginning genetic fitting. Creating population 0...")
+            network_population = [deepcopy(self) for _ in range(population_size)]
+            for network_index, network in enumerate(network_population):
+                for layer_index, layer in enumerate(network.weights):
+                    for k_neuron_index, k_neuron in enumerate(layer):
+                        for j_neuron_index in range(len(k_neuron)):
+                            network_population[network_index].weights[layer_index][k_neuron_index][j_neuron_index] = random.uniform(-1.0, 1.0)
+            print("Beginning the circle of life...")
+            for i in range(num_epochs):
+                print("Determining fitness of population " + str(i))
+                fitnesses = {}
+                min_fitness = None
+                max_fitness = None
+                best_network = None
+                for network_index in range(len(network_population)):
+                    fitness = fitness_callback(network_population[network_index], test_data, test_targets)
+                    if min_fitness is None:
+                        min_fitness = fitness
+                        max_fitness = fitness
+                        best_network = network_population[network_index]
+                    else:
+                        if fitness < min_fitness:
+                            min_fitness = fitness
+                        if fitness > max_fitness:
+                            max_fitness = fitness
+                            best_network = network_population[network_index]
+                    fitnesses[network_population[network_index]] = fitness
+
+
+                self.weights = deepcopy(best_network.weights)
+                train_error = 0
+                test_error = 0
+                for j in range(len(data)):
+                    train_error += (self.predict(data[j]) - targets[j]) ** 2
+                train_error = np.sum(train_error) / len(data)
+                for j in range(len(test_data)):
+                    test_error += (self.predict(test_data[j]) - test_targets[j]) ** 2
+                test_error = np.sum(test_error) / len(test_data)
+
+                plot_details[0].append(i)
+                plot_details[1].append(train_error)
+                plot_details[2].append(test_error)
+
+
+                print("Deciding which networks should survive to breed.")
+                networks_list = []
+                for network_index in range(len(network_population)):
+                    fitnesses[network_population[network_index]] = int(((fitnesses[network_population[network_index]] - min_fitness) / (max_fitness - min_fitness)) * 1000)
+                    for j in range(int(fitnesses[network_population[network_index]])):
+                        networks_list.append(network_population[network_index])
+                print("Performing crossover and mutation...")
+                network_population = []
+                for _ in range(population_size // 4):
+                    net1 = networks_list[random.randint(0, len(networks_list) - 1)]
+                    while net1 in networks_list:
+                        networks_list.remove(net1)
+                    net2 = networks_list[random.randint(0, len(networks_list) - 1)]
+                    while net2 in networks_list:
+                        networks_list.remove(net2)
+                    new_net1 = deepcopy(net1)
+                    new_net2 = deepcopy(net2)
+                    for layer_index in range(len(net1.weights)):
+                        for k_neuron_index in range(len(net1.weights[layer_index])):
+                            for j_neuron_index in range(len(net1.weights[layer_index][k_neuron_index])):
+                                if random.uniform(0,1) <= crossover_rate:
+                                    temp = new_net1.weights[layer_index][k_neuron_index][j_neuron_index]
+                                    new_net1.weights[layer_index][k_neuron_index][j_neuron_index] = new_net2.weights[layer_index][k_neuron_index][j_neuron_index]
+                                    new_net2.weights[layer_index][k_neuron_index][j_neuron_index] = temp
+                                if random.uniform(0,1) <= mutation_rate:
+                                    neg_modifier = (int(random.uniform(0,1) <= .5)-.5) * 2
+                                    new_net1.weights[layer_index][k_neuron_index][j_neuron_index] *= (1 + .01 * neg_modifier)
+                                if random.uniform(0,1) <= mutation_rate:
+                                    neg_modifier = (int(random.uniform(0,1) <= .5)-.5) * 2
+                                    new_net2.weights[layer_index][k_neuron_index][j_neuron_index] *= (1 + .01 * neg_modifier)
+                    network_population.append(net1)
+                    network_population.append(net2)
+                    network_population.append(new_net1)
+                    network_population.append(new_net2)
+            return plot_details
+
+
+
+
 
     def calc_errors_and_update_weights(self, neuron_layer_index, targets=[]):
         """Update weights feeding into the referenced layer of neurons."""
@@ -61,21 +167,22 @@ class Network:
                     i_neuron_activation = self.activations[neuron_layer_index - 1][i_neuron_index]
                     amount_to_change_weight = self.learning_rate * error * i_neuron_activation
                     self.weights_copy[neuron_layer_index - 1][j_neuron_index][i_neuron_index] -= amount_to_change_weight
-                    #print(amount_to_change_weight)
             self.errors = [neuron_layer_errors] + self.errors
         else:
             neuron_layer_errors = []
-            for j_neuron_index in range(len(self.activations[neuron_layer_index])):
+            for j_neuron_index in range(1, len(self.activations[neuron_layer_index])):
                 activation = self.activations[neuron_layer_index][j_neuron_index]
                 weighted_sum_of_previous_errors = 0
-                for k_neuron_index in range(len(self.activations[neuron_layer_index + 1])):
-                    weighted_sum_of_previous_errors += self.errors[0][k_neuron_index]*self.weights[neuron_layer_index][k_neuron_index][j_neuron_index]
+                k_offset_for_bias = 1
+                if neuron_layer_index == len(self.activations)-2:
+                    k_offset_for_bias = 0
+                for k_neuron_index in range(k_offset_for_bias, len(self.activations[neuron_layer_index + 1])):
+                    weighted_sum_of_previous_errors += self.errors[0][k_neuron_index-1]*self.weights[neuron_layer_index][k_neuron_index-1][j_neuron_index]
                 error = activation * (1 - activation) * weighted_sum_of_previous_errors
                 neuron_layer_errors.append(error)
                 for i_neuron_index in range(len(self.activations[neuron_layer_index - 1])):
                     i_neuron_activation = self.activations[neuron_layer_index - 1][i_neuron_index]
                     amount_to_change_weight = self.learning_rate * error * i_neuron_activation
-                    self.weights_copy[neuron_layer_index - 1][j_neuron_index][i_neuron_index] -= amount_to_change_weight
-                    #print(amount_to_change_weight)
+                    self.weights_copy[neuron_layer_index - 1][j_neuron_index-1][i_neuron_index] -= amount_to_change_weight
             self.errors = [neuron_layer_errors] + self.errors
 
